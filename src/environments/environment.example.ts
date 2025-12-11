8da96924966bea2f61213b3df64170237faaaf15
Dasha Shevlya <shevliakova.d@gmail.com>
2025-12-11 11:47:40 +0400

    инструкция настройки

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,6 +1,5 @@
 //!!Если у Вас нет предыдущей версии, то создайте файл environment.ts
-//Шаблон файла расположен ниже
-
+//ЭТО ШАБЛОН файла
 export const environment = {
   production: false,
   apiUrl: 'http://-.-.-.-:8080'
