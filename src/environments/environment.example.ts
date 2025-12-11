0c59446a862314cf21a18fb99a8ef3fc8f73d2cc
Dasha Shevlya <shevliakova.d@gmail.com>
2025-12-11 11:42:12 +0400

    добавляем на всякий случай шаблон файла environment.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+//!!Если у Вас нет предыдущей версии, то создайте файл environment.ts
+//Шаблон файла расположен ниже
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://-.-.-.-:8080'
+};
